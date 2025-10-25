6cc7c944fa8df4cf511cc42db90ff0a3f4f0959d
Jawat Al Sovon <80356626+jawatalsovon@users.noreply.github.com>
2025-10-25 14:02:04 +0000

    Sat, Oct 25, 2025, 8:02 PM +06:00

--- app_3.py
+++ app_3.py
@@ -68,7 +68,9 @@ CROP_HIERARCHY = {
 }
 
 # Crops for which we currently have data tables
-AVAILABLE_MAJOR_CROPS = ["Aus Rice", "Aman Rice", "Boro Rice", "Wheat", "Maize", "Jower (Millet)", "Barley/Jab", "Cheena & Kaon", "Binnidana", "Lentil (Masur)", "Kheshari", "Mashkalai", "Mung", "Gram", "Motor", "Fallon", "Other Pulses"]
+AVAILABLE_MAJOR_CROPS = ["Aus Rice", "Aman Rice", "Boro Rice", "Wheat", "Maize", "Jower (Millet)", "Barley/Jab", "Cheena & Kaon", "Binnidana", "Lentil (Masur)",
+                         "Kheshari", "Mashkalai", "Mung", "Gram", "Motor", "Fallon", "Other Pulses", "Rape and Mustard", "Til", "Groundnut", "Soyabean", "Linseed", "Coconut", "Sunflower",
+                         ]
 
 # --- FLASK ROUTES ---
 
