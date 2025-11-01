625f7e9c795b5ae55316a58e887e892c0e934dd9
Jawat Al Sovon <80356626+jawatalsovon@users.noreply.github.com>
2025-11-01 21:13:49 +0000

    Sun, Nov 2, 2025, 3:13 AM +06:00

--- download_pcos.sh
+++ download_pcos.sh
@@ -1,12 +1,3 @@
-# URL of the article page
-SDURL="https://www.sciencedirect.com/science/article/abs/pii/S2468784725001163?via=ihub"
-
-# Step 1: Fetch the HTML and extract the PDF URL
-curl -s -L -c cookiejar -b cookiejar "$SDURL" \
-  | grep -oP 'pdfurl="[^"]+"' \
-  | sed -e 's/pdfurl="//' -e 's/"//' \
-  > pdfurl.txt
-
-# Step 2: Download the PDF using the extracted URL
-PDFURL=$(cat pdfurl.txt)
-curl -L -c cookiejar -b cookiejar "https://www.sciencedirect.com${PDFURL}" -o article.pdf
+export SDURL="http://www.sciencedirect.com/science/article/pii/S0169433215012131"
+curl -Lc cookiejar "${SDURL}" | grep pdfurl | perl -pe 's|.* pdfurl=\"(.*?)\".*|\1|' > pdfurl
+curl -Lc cookiejar "$(cat pdfurl)" > article.pdf
